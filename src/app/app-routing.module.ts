42c8aed9f44972221817841ad1c55f79ce65fca1
Shosetsu <Shosetsu.rr@outlook.com>
2023-04-09 20:59:08 +0800

    删除不要的示例代码，并添加了gh-pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,18 +4,6 @@ import { MenuComponent } from './pages/menu/menu.component';
 
 const routes: Routes = [
   {
-    path: 'lazy1',
-    loadChildren: async () =>
-      (await import('./pages/lazy-menu/lazy-menu.module')).LazyMenuModule,
-  },
-  {
-    path: 'lazy2/:aaa',
-    loadChildren: () =>
-      import('./pages/lazy-menu/lazy-menu.module').then(
-        (m) => m.LazyMenuModule
-      ),
-  },
-  {
     title: 'Angular指导目录',
     path: '',
     component: MenuComponent,
