3b5b122ffe0f009c2b66ac94c13e88f9c813e211
Shosetsu <Shosetsu.rr@outlook.com>
2023-04-14 00:38:06 +0800

    停止zone对scroll和mousemove的处理

--- src/main.ts
+++ src/main.ts
@@ -4,3 +4,6 @@ import { AppModule } from './app/app.module';
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
   .catch((err) => console.error(err));
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+(window as any).__zone_symbol__UNPATCHED_EVENTS = ['scroll', 'mousemove'];
