ac535eef23a37a5e539f444050491c5d750674e3
Shosetsu <Shosetsu.rr@outlook.com>
2023-04-14 22:31:27 +0800

    修一个inject错误

--- src/app/pages/sample/sample1.component.ts
+++ src/app/pages/sample/sample1.component.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import { Component, NgZone, OnInit, inject } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-sample1',
@@ -12,9 +12,8 @@ export class Sample1Component implements OnInit {
 
   sample1Value = '1';
 
-  constructor() {
-    (window as any).runInside = (fn: () => void) =>
-      inject(NgZone).run(() => fn());
+  constructor(private ngZone: NgZone) {
+    (window as any).runInside = (fn: () => void) => this.ngZone.run(() => fn());
     (window as any).isInside = () => NgZone.isInAngularZone();
     (window as any).updateSample1Value = (val: string) =>
       (this.sample1Value = val);
