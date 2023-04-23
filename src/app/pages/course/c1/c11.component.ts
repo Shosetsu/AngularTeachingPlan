b70baae5fc7584d3c04b1b4ebe47e2f3756c2c69
Shosetsu <Shosetsu.rr@outlook.com>
2023-04-23 18:42:45 +0800

    添加一些第二章课程内容，并略微修改代码组件的样式

---
+++ src/app/pages/course/c1/c11.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+import { CourseDetailComponent } from '../course-detail.component';
+
+@Component({
+  selector: 'app-c11',
+  templateUrl: './c11.component.html',
+})
+export class C11Component extends CourseDetailComponent {}
--- src/app/pages/course/course.module.ts
+++ src/app/pages/course/course.module.ts
@@ -4,15 +4,16 @@ import { PipesModule } from '@pipes/_pipes.module';
 import { CommonModule } from '@angular/common';
 import { ComponentsModule } from '@components/_components.module';
 import { CourseComponent } from '@pages/course/course.component';
+import { RouterModule } from '@angular/router';
+import { courseKeyList, getTitleByRoute } from '@app/configs/util';
 import { C00Component } from './c0/c00.component';
 import { C01Component } from './c0/c01.component';
 import { C02Component } from './c0/c02.component';
 import { C03Component } from './c0/c03.component';
 import { C04Component } from './c0/c04.component';
-import { C51Component } from './c5/c51.component';
 import { C10Component } from './c1/c10.component';
-import { RouterModule } from '@angular/router';
-import { courseKeyList, getTitleByRoute } from '@app/configs/util';
+import { C11Component } from './c1/c11.component';
+import { C51Component } from './c5/c51.component';
 
 const courseComponents = [
   C00Component,
@@ -21,6 +22,7 @@ const courseComponents = [
   C03Component,
   C04Component,
   C10Component,
+  C11Component,
   C51Component,
 ];
 
