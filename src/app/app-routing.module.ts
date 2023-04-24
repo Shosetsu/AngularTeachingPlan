f2ff2411508d327db83f5c5328ee607865752511
Shosetsu <Shosetsu.rr@outlook.com>
2023-04-24 18:04:51 +0800

    教学进度更新

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, inject } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Router, RouterModule, Routes } from '@angular/router';
 import { MenuComponent } from './pages/menu/menu.component';
 import { isChildWindow } from './configs/util';
 import { DataService } from './shared/data.service';
@@ -29,8 +29,9 @@ const routes: Routes = [
     canActivate: [
       () => {
         localStorage['atp-data'] =
-          '{"libCheck.c0-0-1":"1","movable.modal":"27px,530px","resizable.modal":{"left":"54px","right":"908px","top":"309px","bottom":"555px"},"libCheck.c0-0-2":"1","libCheck.c0-0-3":"1","libCheck.c0-0-4":"1","courseStatus":{"c0-0":1,"c0-1":1,"c0-2":1,"c0-3":1,"c0-4":1},"libCheck.c0-1-1":"1","libCheck.c0-1-2":"1","libCheck.c0-2-1":"1","libCheck.c0-2-2":"1","libCheck.c0-2-3":"1","libCheck.c0-3-1":"1","libCheck.c0-3-5":"1","libCheck.c0-3-3":"1","libCheck.c0-3-4":"1","libCheck.c0-3-2":"1","libCheck.c0-3-6":"1","libCheck.c0-4-1":"1","libCheck.c0-4-2":"1"}';
+          '{"libCheck.c0-0-1":"1","movable.modal":"13px,894px","resizable.modal":{"left":"54px","right":"908px","top":" 300px","bottom":"555px"},"libCheck.c0-0-2":"1","libCheck.c0-0-3":"1","libCheck.c0-0-4":"1","courseStatus":{"c0-0":1,"c0-1":1,"c0-2":1,"c0-3":1,"c0-4":1,"c1-0":1,"c1-1":1,"c1-5":1},"libCheck.c0-1-1":"1","libCheck.c0-1-2":"1","libCheck.c0-2-1":"1","libCheck.c0-2-2":"1","libCheck.c0-2-3":"1","libCheck.c0-3-1":"1","libCheck.c0-3-5":"1","libCheck.c0-3-3":"1","libCheck.c0-3-4":"1","libCheck.c0-3-2":"1","libCheck.c0-3-6":"1","libCheck.c0-4-1":"1","libCheck.c0-4-2":"1","libCheck.c1-0-1":"1","libCheck.c1-0-2":"1","libCheck.c1-1-1":"1","libCheck.c1-1-2":"1","libCheck.c1-1-3":"1","libCheck.c1-1-4":"1","libCheck.c1-1-5":"1","libCheck.c1-1-6":"1","libCheck.c1-1-7":"1","libCheck.c1-1-8":"1","libCheck.c1-1-9":"1","libCheck.c1-1-10":"1","libCheck.c1-1-11":"1","libCheck.c1-1-12":"1"}';
         inject(DataService).loadData();
+        return inject(Router).parseUrl('/');
       },
     ],
     component: MenuComponent,
