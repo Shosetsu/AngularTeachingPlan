0442b6b9b5bef4fc415d2ee83e77c58c745e45cf
Shosetsu <Shosetsu.rr@outlook.com>
2023-04-25 23:43:02 +0800

    小修改

--- src/app/pages/sample/sample-component/lifecycle.component.ts
+++ src/app/pages/sample/sample-component/lifecycle.component.ts
@@ -3,7 +3,6 @@ import {
   AfterContentInit,
   AfterViewChecked,
   AfterViewInit,
-  ChangeDetectionStrategy,
   Component,
   DoCheck,
   Input,
@@ -43,50 +42,52 @@ export class LifecycleComponent
   @Input() memoried!: string[];
 
   ngOnChanges(changes: SimpleChanges): void {
-    this.memoried.push(
-      `${new Date().toLocaleTimeString()} 发生了事件[OnChanges] : ${JSON.stringify(
-        changes
-      )}`
-    );
+    const log = `${new Date().toLocaleTimeString()} 发生了事件[OnChanges] : ${JSON.stringify(
+      changes
+    )}`;
+    console.log(log);
+    this.memoried.push(log);
   }
 
   ngOnInit(): void {
-    this.memoried.push(`${new Date().toLocaleTimeString()} 发生了事件[OnInit]`);
+    const log = `${new Date().toLocaleTimeString()} 发生了事件[OnInit]`;
+    console.log(log);
+    this.memoried.push(log);
   }
 
   ngDoCheck(): void {
-    this.memoried.push(
-      `${new Date().toLocaleTimeString()} 发生了事件[DoCheck]`
-    );
+    const log = `${new Date().toLocaleTimeString()} 发生了事件[DoCheck]`;
+    console.log(log);
+    this.memoried.push(log);
   }
 
   ngAfterContentInit(): void {
-    this.memoried.push(
-      `${new Date().toLocaleTimeString()} 发生了事件[AfterContentInit]`
-    );
+    const log = `${new Date().toLocaleTimeString()} 发生了事件[AfterContentInit]`;
+    console.log(log);
+    this.memoried.push(log);
   }
 
   ngAfterContentChecked(): void {
-    this.memoried.push(
-      `${new Date().toLocaleTimeString()} 发生了事件[AfterContentChecked]`
-    );
+    const log = `${new Date().toLocaleTimeString()} 发生了事件[AfterContentChecked]`;
+    console.log(log);
+    this.memoried.push(log);
   }
 
   ngAfterViewInit(): void {
-    this.memoried.push(
-      `${new Date().toLocaleTimeString()} 发生了事件[AfterViewInit]`
-    );
+    const log = `${new Date().toLocaleTimeString()} 发生了事件[AfterViewInit]`;
+    console.log(log);
+    this.memoried.push(log);
   }
 
   ngAfterViewChecked(): void {
-    this.memoried.push(
-      `${new Date().toLocaleTimeString()} 发生了事件[AfterViewChecked]`
-    );
+    const log = `${new Date().toLocaleTimeString()} 发生了事件[AfterViewChecked]`;
+    console.log(log);
+    this.memoried.push(log);
   }
 
   ngOnDestroy(): void {
-    this.memoried.push(
-      `${new Date().toLocaleTimeString()} 发生了事件[OnDestroy]`
-    );
+    const log = `${new Date().toLocaleTimeString()} 发生了事件[OnDestroy]`;
+    console.log(log);
+    this.memoried.push(log);
   }
 }
