bf4d21fc63350253c823b7e6b26150f115e38dd2
Shosetsu <Shosetsu.rr@outlook.com>
2023-04-26 17:21:01 +0800

    无用逻辑删除

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavigationEnd, NavigationStart, Router } from '@angular/router';
 
 @Component({
@@ -6,7 +6,7 @@ import { NavigationEnd, NavigationStart, Router } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   /** 加载指示符 */
   isLoader = false;
 
@@ -16,25 +16,18 @@ export class AppComponent implements OnInit {
    * @param router 路由管理服务
    * @param modal 窗口管理服务
    */
-  constructor(private router: Router) {}
-
-  /**
-   * 程序初始处理
-   */
-  ngOnInit(): void {
+  constructor(private router: Router) {
     // 预先绑定路由事件
     this.router.events.subscribe((event) => {
       switch (true) {
         // 开始导航时添加屏幕锁定
         case event instanceof NavigationStart: {
           this.isLoader = true;
-          document.body.classList.add('load');
           break;
         }
         // 结束导航时解除屏幕锁定
         case event instanceof NavigationEnd: {
           this.isLoader = false;
-          document.body.classList.remove('load');
           break;
         }
       }
